7e4baa3b9d82ed117758227e8a8d9a6c2e71c4f0
AshvinAA <ashvin.afroz@gmail.com>
2026-01-29 19:18:26 +0600

    Login/Registration form complete

--- flaskblog.py
+++ flaskblog.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8e4ef53df82522c718d6d97abd4941b4'
+
+posts=[
+    {
+        'author':'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted':'April 20,2018'
+    },
+    {
+        'author':'Ashvin Afroz',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted':'April 22,2018'
+    }
+]
 
 
 @app.route("/")
-def hello():
-	return "<h1> Hello World! </h1>"
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html",title='about')
 
 
 if __name__ == "__main__":
-	app.run(debug=True) 
+    app.run(debug=True)
