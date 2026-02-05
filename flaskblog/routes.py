c40c038356f776206132d751038d8cbb7ffbe7d4
AshvinAA <ashvin.afroz@gmail.com>
2026-02-06 03:46:29 +0600

    profile route html and picture

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -74,4 +74,5 @@ def logout():
 @login_required
 #login_required checks if the person is logged in or not
 def account():
-    return render_template('account.html' , title='Account')
+    image_file= url_for('static' , filename= 'profile_pics/' + current_user.image_file)
+    return render_template('account.html' , title='Account',image_file = image_file)
